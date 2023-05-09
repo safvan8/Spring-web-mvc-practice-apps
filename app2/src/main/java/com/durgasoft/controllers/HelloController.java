05117bc69e57084f501dc43a15d61feadc9ff303
Safvan <safvanpmcz@gmail.com>
2023-05-09 12:15:40 +0530

    2 controller 2 JSP file project

---
+++ app2/src/main/java/com/durgasoft/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.durgasoft.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements Controller
+{
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		ModelAndView modelAndView = new ModelAndView("hello"); // hello is view name
+		
+		return modelAndView;
+	}
+}
