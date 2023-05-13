6085921e8a3e106bbf6a9e63346e42c3ec470f58
Safvan <safvanpmcz@gmail.com>
2023-05-14 04:58:07 +0530

    returning void from Controller class method

---
+++ All-sprinboot-mvc-projects/bootMVCProj5-WishMessageApp_Using_return_void/src/main/java/in/ineuron/controller/WishMessageController.java
@@ -0,0 +1,36 @@
+package in.ineuron.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import in.ineuron.service.IWishMessageService;
+
+@Controller
+public class WishMessageController {
+
+	@Autowired
+	private IWishMessageService service;
+
+	// configuring index page
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String showHome() {
+		return "home";
+	}
+
+	@RequestMapping(value = "/wish", method = RequestMethod.GET)
+	public void getWishMessage(Map map) {
+
+		// collect business result from service
+		String msg = service.generateWishMessage();
+
+		// saving Model class objects to Map
+		map.put("wmg", msg);
+		
+		
+		// will go to WEB-INF/pages/wish.jsp
+	}
+}
